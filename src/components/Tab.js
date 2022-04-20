6dec1f7f8a7dd63c5af192f2b8d6c4c624e7e372
lazy-sky <1cielo@naver.com>
2022-04-20 21:14:43 +0900

    feat: Make Tab

---
+++ src/components/Tab.js
@@ -0,0 +1,76 @@
+import { useState } from 'react';
+import styled, { css, keyframes } from 'styled-components';
+
+function Tab({ items, title, currentTabIndex }) {
+  const [activeTabIndex, setActiveTabIndex] = useState(currentTabIndex);
+  const [previousTabIndex, setPreviousTabIndex] = useState(null);
+
+  const handleClickTab = (index) => {
+    setPreviousTabIndex(activeTabIndex);
+    setActiveTabIndex(index);
+  };
+
+  return (
+    <>
+      <h2>{title}</h2>
+      <TabList>
+        {items.map((item, index) => (
+          <TabItem
+            key={index}
+            index={index}
+            previousTabIndex={previousTabIndex}
+            isActive={activeTabIndex === index}
+            onClick={() => handleClickTab(index)}
+          >
+            {item}
+          </TabItem>
+        ))}
+      </TabList>
+    </>
+  );
+}
+
+const TabList = styled.ul`
+  display: flex;
+  justify-content: center;
+  list-style: none;
+`;
+
+const TabItem = styled.li`
+  font-weight: bold;
+  padding: 12px;
+  text-align: center;
+  width: 120px;
+  cursor: pointer;
+  position: relative;
+  outline: none;
+  color: gray;
+  border-bottom: 2px solid rgba(0, 0, 0, 0.1);
+  ${({ previousTabIndex, index, isActive }) =>
+    isActive &&
+    css`
+      color: black;
+      &::before {
+        content: '';
+        display: block;
+        position: absolute;
+        bottom: -2px;
+        left: 0;
+        width: 100%;
+        height: 2px;
+        background: rgb(16, 175, 175);
+        animation: ${slide(previousTabIndex, index)} 0.2s linear;
+      }
+    `}
+`;
+
+const slide = (previousTabIndex, index) => keyframes`
+  from {
+    transform: translateX(${(previousTabIndex - index) * 100}%);
+  }
+  to {
+    transform: translateX(0px);
+  }
+`;
+
+export default Tab;

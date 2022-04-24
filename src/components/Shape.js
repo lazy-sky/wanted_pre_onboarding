4152a220a99343b832db7acd37934d81a8756830
lazy-sky <1cielo@naver.com>
2022-04-24 20:46:15 +0900

    feat: Get width props in Shape

--- src/components/Shape.js
+++ src/components/Shape.js
@@ -1,8 +1,8 @@
 import styled from 'styled-components';
 
 export const Container = styled.div`
-  width: 300px;
+  width: ${({ width }) => width || '300px'};
   margin: 20px auto;
   padding: 8px;
-  background-color: ${props => props.backgroundColor || 'white'};
+  background-color: ${({ backgroundColor }) => backgroundColor || 'white'};
 `;

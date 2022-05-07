9351e6478e1bf71266c161e202fcdb533a1a6cc2
lazy-sky <1cielo@naver.com>
2022-05-07 23:30:03 +0900

    refact: Redesign Tab with scss

--- src/components/Tab/index.js
+++ src/components/Tab/index.js
@@ -1,15 +1,14 @@
+import classNames from 'classnames'
 import PropTypes from 'prop-types'
 import { useState } from 'react'
-import styled, { css, keyframes } from 'styled-components'
 
 import { Container } from '../Shape'
+import styles from './Tab.module.scss'
 
 function Tab({ items, title, currentTabIndex }) {
   const [activeTabIndex, setActiveTabIndex] = useState(currentTabIndex)
-  const [previousTabIndex, setPreviousTabIndex] = useState(null)
 
   const handleClickTab = (index) => {
-    setPreviousTabIndex(activeTabIndex)
     setActiveTabIndex(index)
   }
 
@@ -17,19 +16,19 @@ function Tab({ items, title, currentTabIndex }) {
     <>
       <h2>{title}</h2>
       <Container width='500px'>
-        <TabList>
+        <ul className={styles.tabList}>
           {items.map((item, index) => (
-            <TabItem
+            <li
+              className={classNames(styles.tabItem, activeTabIndex === index && styles.active)}
               key={item}
               index={index}
-              previousTabIndex={previousTabIndex}
-              isActive={activeTabIndex === index}
-              onClick={() => handleClickTab(index)}
             >
-              {item}
-            </TabItem>
+              <button type='button' onClick={() => handleClickTab(index)}>
+                {item}
+              </button>
+            </li>
           ))}
-        </TabList>
+        </ul>
       </Container>
     </>
   )
@@ -40,48 +39,5 @@ Tab.propTypes = {
   title: PropTypes.string,
   currentTabIndex: PropTypes.number,
 }
-
-const TabList = styled.ul`
-  display: flex;
-  justify-content: center;
-  list-style: none;
-`
-
-const slide = (previousTabIndex, index) => keyframes`
-  from {
-    transform: translateX(${(previousTabIndex - index) * 100}%);
-  }
-  to {
-    transform: translateX(0px);
-  }
-`
-
-const TabItem = styled.li`
-  font-weight: bold;
-  padding: 12px;
-  text-align: center;
-  width: 120px;
-  cursor: pointer;
-  position: relative;
-  outline: none;
-  color: gray;
-  border-bottom: 2px solid rgba(0, 0, 0, 0.1);
-  ${({ previousTabIndex, index, isActive }) =>
-    isActive &&
-    css`
-      color: black;
-      &::before {
-        content: '';
-        display: block;
-        position: absolute;
-        bottom: -2px;
-        left: 0;
-        width: 100%;
-        height: 2px;
-        background: rgb(16, 175, 175);
-        animation: ${slide(previousTabIndex, index)} 0.2s linear;
-      }
-    `}
-`
 
 export default Tab
